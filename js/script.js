210714c68e2a7ea7f5f7e1f17e66b1e89cd5d4ed
giovani <giovanimanzattof@gmail.com>
2025-12-14 03:14:19 -0300

    e

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ window.addEventListener('scroll', () => {
         header.classList.remove('active');
     }
 });
+
+function enviar() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const data = document.getElementById('data').value;
+    const hora = document.getElementById('hora').value;
+    const whats = `19995937457`
+   
+
+    if (nome === '' || email === '' || mensagem === '' || data === '' || hora === '') {
+        alert('Preencha todos os campos');
+        return;
+    }
+
+    let mensagemFormatada = `Olá, Bom dia! Eu gostaria de marcar um horário com você.
+    Nome: ${nome}\nEmail: ${email}\nMensagem: ${mensagem}\nData: ${data}\nHora: ${hora}`;
+    window.open(`https://wa.me/${whats}?text=${encodeURIComponent(mensagemFormatada)}`);
+    
+
+}
